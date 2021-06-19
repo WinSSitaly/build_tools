46a1f9bae791384d43d4770b5b7fcbea0997146b
1v4n_1976 <giglione@libero.it>
2021-06-19 08:31:42 +0200

    preparo

---
+++ prepara.sh
@@ -0,0 +1,30 @@
+mkdir ./device/samsung
+mkdir ./kernel/samsung
+mkdir ./vendor/samsung
+
+pwd
+
+#device
+cd device/samsung
+
+pwd
+
+git clone https://github.com/WinSSitaly/android_device_samsung_galaxys2plus-common -b cm-14.1
+git clone https://github.com/WinSSitaly/android_device_samsung_i9105p -b cm-14.1
+cd ../../
+#kernel
+cd kernel/samsung
+
+pwd
+
+git clone https://github.com/WinSSitaly/android_kernel_samsung_galaxys2plus-common-5.1 -b  android-7.1
+cd ../../
+#vendor
+cd vendor/samsung
+
+pwd
+
+git clone https://github.com/WinSSitaly/proprietary_vendor_samsung_galaxys2plus-common -b  master
+cd ../../
+
+pwd
