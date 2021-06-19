d2e88169b1d8c18ee5053c5b6c2fcd07fbad3dfa
1v4n_1976 <giglione@libero.it>
2021-06-19 13:41:47 +0200

    update prepara.sh

--- prepara.sh
+++ prepara.sh
@@ -1,32 +1,35 @@
 #prepare the root for device kernel and vendor
 cd ..
+mkdir ./device
+mkdir ./kernel
+mkdir ./vendor
 mkdir ./device/samsung
 mkdir ./kernel/samsung
 mkdir ./vendor/samsung
 
-pwd
+#pwd
 
 #device
 cd device/samsung
 
-pwd
+#pwd
 
-git clone https://github.com/WinSSitaly/android_device_samsung_galaxys2plus-common -b cm-14.1
-git clone https://github.com/WinSSitaly/android_device_samsung_i9105p -b cm-14.1
+git clone https://github.com/WinSSitaly/android_device_samsung_galaxys2plus-common ./galaxys2plus-common -b cm-14.1
+git clone https://github.com/WinSSitaly/android_device_samsung_i9105p ./i9105p -b cm-14.1
 cd ../../
 #kernel
 cd kernel/samsung
 
-pwd
+#pwd
 
-git clone https://github.com/WinSSitaly/android_kernel_samsung_galaxys2plus-common-5.1 -b  android-7.1
+git clone https://github.com/WinSSitaly/android_kernel_samsung_galaxys2plus-common ./galaxys2plus-common -b  android-7.1
 cd ../../
 #vendor
 cd vendor/samsung
 
-pwd
+#pwd
 
-git clone https://github.com/WinSSitaly/proprietary_vendor_samsung_galaxys2plus-common -b  master
+git clone https://github.com/WinSSitaly/proprietary_vendor_samsung_galaxys2plus-common ./galaxys2plus-common -b  master
 cd ../../
 
-pwd
+cd build_tools
